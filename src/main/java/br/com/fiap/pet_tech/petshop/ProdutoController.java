d018753c25d77ed2278303ba85582ae7e2b7687a
WendelSReis <wendel.m.w18@gmail.com>
2024-07-15 22:22:25 -0300

    Rest GET metodo

---
+++ src/main/java/br/com/fiap/pet_tech/petshop/ProdutoController.java
@@ -0,0 +1,29 @@
+package br.com.fiap.pet_tech.petshop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public ResponseEntity<Collection<Produto>> findAll() {
+        var produtos = service.findAll();
+        return ResponseEntity.ok(produtos);
+    }
+    @GetMapping("/id")
+    public ResponseEntity<Optional<Produto>> findById(@PathVariable UUID id) {
+        var produto = service.findById(id);
+        return ResponseEntity.ok(produto);
+    }
+}
